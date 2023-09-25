e7114551597740aed9dca99c1bb1cf74a9aad434
REKA309 <reka3092@gmail.com>
2023-09-25 07:56:06 +0530

    preview pdf commit changes

--- src/Components/projectSection.js
+++ src/Components/projectSection.js
@@ -6,25 +6,24 @@ export default function Projects()
 {
   function handleDocs()
   {
-     // replace this URL with the URL of your resume PDF file
-     const manualUrl = 'https://docs.google.com/document/d/1OlDbaqv_6UBaKcczPeVdhdLrzFKBGFCeWKzyWFM0IAA/edit?usp=sharing';
-        
-     // create a new anchor element
-     const downloadLink = document.createElement('a');
-     
-     // set the href attribute of the anchor to the URL of your resume PDF file
-     downloadLink.href = manualUrl;
-     downloadLink.target='_blank';
-     
-     // set the download attribute of the anchor to the filename you want to use for the downloaded file
-     downloadLink.download = 'manual.doc';
-     
-     // simulate a click on the anchor element to trigger the download
-     downloadLink.click();
+    const previewUrl = 'https://drive.google.com/file/d/1blwf30vI_OVfnJ8dKcATOSXYTL6Fzf1H/view?usp=sharing';
+      
+    // create a new anchor element
+    const downloadLink = document.createElement('a');
+    
+    // set the href attribute of the anchor to the URL of your resume PDF file
+    downloadLink.href = previewUrl;
+    downloadLink.target='_blank';
+    
+    // set the download attribute of the anchor to the filename you want to use for the downloaded file
+    downloadLink.download = 'preview.pdf';
+    
+    // simulate a click on the anchor element to trigger the download
+    downloadLink.click();
   }
     return(
         <div className="project-container row">
-            <h1><b>Projects 💻</b><Button color="inherit" onClick={handleDocs}>DOCS CLICK HERE</Button></h1>
+            <h1><b>Projects 💻</b><Button color="inherit" onClick={handleDocs}>PREVIEW CLICK HERE</Button></h1>
            {projects.map((project,index)=>(
               <div key={index} className="card card1 col-lg-4  col-md-6 col-sm-12">
                 <img src={project.thumbnail} alt={project.altname} width={264} height={300}/>
